dcc5fd121c49972dd2a4c216d3920dce5f30bb60
Tomas <drasutis.tomas@gmail.com>
2019-10-23 14:02:49 +0300

    Moved to .net framework and added some classes that will need to be implemented.

---
+++ Reed-Muler-Code/UnitTests/EncoderTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class EncoderTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Encode_Succeeds()
+        {
+            Assert.Pass();
+        }
+    }
+}
